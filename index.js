d8aa69a9c664d1724ede7e02c6fd2cab53f8d7ed
unknown <jradegra@PCJRADEGRAT410.emea.tibco.com>
2015-07-14 08:49:16 +0200

    net.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-$(document).ready(function() 
-{
-	$.ajax({
-		type: "POST",
-		url: "/create",
-		data: JSON.stringify({}),
-		contentType: "application/json",
-		success: function(d) 
-		{ 
-			window.location.href = "/" + d.url; 
-		}
-	});
-
-});
+$(document).ready(function() { createNewApp() });
