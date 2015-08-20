12f013857f96adb8f13ea8c9c834c487f9811dc4
unknown <jradegra@PCJRADEGRAT410.emea.tibco.com>
2015-08-20 07:00:35 +0200

    New startpage colors

--- client/dist/loadapp.js
+++ client/dist/loadapp.js
@@ -1,18 +1,16 @@
 $(document).ready(function()
 {
-	var $page = $("<div/>").addClass("flex-horizontal-container flex-justify-center").append(
-		$("<div/>").addClass("flex-vertical-container").append(
-			$("<div/>").html(L.Welcome1),
-			$("<div/>").html(L.Welcome2),
-			$("<div/>").html("&nbsp;"),
-			$("<div/>").html("&nbsp;"),
-			$("<div/>").html("&nbsp;"),
-			$("<div/>").addClass("flex-horizontal-container flex-justify-center").append(
-				$("<div/>").addClass("nav-payments").on("click", loadApp)
+	var $page = vertical().append(
+		horizontal("ui-navigation-bar").append(
+			vertical().css("padding", "1.5em").append(
+				div().html(L.Welcome1),
+				div().html(L.Welcome2)		
 			)
+		),
+		horizontal().css("padding", "2em").append(
+			div("nav-payments").on("click", loadApp)
 		)
 	);
 	
 	$(document.body).append($page);
-	$(document.body).css("margin", "2em");
 });
