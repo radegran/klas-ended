fc8f9f3626aa6f2600b044d230297a1a244d41db
Jesper Rådegran <jradegra@tibco.com>
2017-09-06 15:08:35 +0200

    Updated nodejs env variable names

--- server/server.js
+++ server/server.js
@@ -202,13 +202,13 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = '0.0.0.0'; //process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = 8080; //process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.ipaddress = process.env.NODEJS_MONGO_PERSISTENT_SERVICE_HOST;
+        self.port      = process.env.NODEJS_MONGO_PERSISTENT_SERVICE_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            console.warn('No NODEJS_MONGO_PERSISTENT_SERVICE_HOST var, using 127.0.0.1');
             self.ipaddress = "0.0.0.0" || "127.0.0.1";
 			self.isDevelEnv = true;
         };
